906ebfec9114f0ea913d5f4bb3c28ea8b62312ba
ragul2k23 <rockyragulsiva2005@gmail.com>
2023-11-29 23:43:52 +0530

    Asset allocation
    Learned how to import images and work with bundling up
    things and got familiar with web dev server
    
    Still have to work on the project .

---
+++ main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('dsra');
+let curd = document.getElementById('con');
+let h1 = document.createElement('h1');
+h1.textContent="wondering how u are working now why are u even working now after wasting my so much of time ahh?\
+";
+curd.append(h1);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxnQiIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stZGVtby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnZHNyYScpO1xyXG5sZXQgY3VyZCA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdjb24nKTtcclxubGV0IGgxID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaDEnKTtcclxuaDEudGV4dENvbnRlbnQ9XCJ3b25kZXJpbmcgaG93IHUgYXJlIHdvcmtpbmcgbm93IHdoeSBhcmUgdSBldmVuIHdvcmtpbmcgbm93IGFmdGVyIHdhc3RpbmcgbXkgc28gbXVjaCBvZiB0aW1lIGFoaD9cXFxyXG5cIjtcclxuY3VyZC5hcHBlbmQoaDEpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
